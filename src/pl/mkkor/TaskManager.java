357e841756595ace76b557efbdf2704d2fa195fd
Maciej Korczala <maciej.coders@gmail.com>
2021-07-13 22:08:49 +0200

    minor corrections in methods and display

--- src/pl/mkkor/TaskManager.java
+++ src/pl/mkkor/TaskManager.java
@@ -20,28 +20,28 @@ public class TaskManager {
         try {
             dataFromFileArray = readDataFromFile(csvFile);
         } catch (FileNotFoundException e) {
-            System.out.println("Given file: \'" + csvFile + "\' has not been found. Check file directory");
+            System.out.println("Given file: '" + csvFile + "' has not been found. Check file directory");
             return;
         }
-        System.out.println("TEST: " + Arrays.deepToString(dataFromFileArray));
-
-        while(true) {
-            String[] optionsToSelect = displayOptions();
+        String[] optionsToSelect = new String[]{"add", "remove", "list", "exit"};
+        while (true) {
+            displayOptions(optionsToSelect);
             String chosenOption = choosingOption(optionsToSelect);
 
             switch (chosenOption) {
                 case "add":
                     dataFromFileArray = add(dataFromFileArray);
-                    break;
+                    break;      //NEW break nie przerywa while'a
                 case "remove":
                     dataFromFileArray = remove(dataFromFileArray);
                     break;
                 case "list":
                     list(dataFromFileArray);
                     break;
-                case "exit": exit(dataFromFileArray, csvFile);
-                    System.out.println(RED + "Bye, bye");
-                    return;
+                case "exit":
+                    exit(dataFromFileArray, csvFile);
+                    System.out.println(PURPLE_BRIGHT + "Bye, bye");
+                    return;           //NEW działa na main
             }
         }
     }
@@ -59,13 +59,11 @@ public class TaskManager {
         return dataFromFileArray;
     }
 
-    private static String[] displayOptions() {
-        String[] optionsToSelect = new String[]{"add", "remove", "list", "exit"};
+    private static void displayOptions(String[] optionsToSelect) {
         System.out.println(BLUE + "Please select an option (choose only one option):" + RESET);
         for (String row : optionsToSelect) {
             System.out.println(row);
         }
-        return optionsToSelect;
     }
 
     private static String choosingOption(String[] optionsToSelect) {
@@ -76,7 +74,7 @@ public class TaskManager {
             if (!StringUtils.equalsAnyIgnoreCase(chosenOption, optionsToSelect[0], optionsToSelect[1],
                     optionsToSelect[2], optionsToSelect[3])) {
                 System.out.println(RED + "Option chosen by you is not supported by this app. ");
-                displayOptions();
+                displayOptions(optionsToSelect);
                 continue;
             }
             break;
@@ -86,58 +84,59 @@ public class TaskManager {
 
     private static String[][] add(String[][] dataFromFileArray) {
         Scanner scan = new Scanner(System.in);
-        dataFromFileArray = Arrays.copyOf(dataFromFileArray, dataFromFileArray.length+1);  //differently than in readDataFromFile() method, I don't use addAll from ArrayUtils here;
-        dataFromFileArray[dataFromFileArray.length-1] = new String[3];  //NEW muszę inicjalizować 2. wymiar, bez tego jt null
+        dataFromFileArray = Arrays.copyOf(dataFromFileArray, dataFromFileArray.length + 1);  //differently than in readDataFromFile() method, I don't use addAll from ArrayUtils here;
+        dataFromFileArray[dataFromFileArray.length - 1] = new String[3];  //NEW muszę inicjalizować 2. wymiar, bez tego jt null
         System.out.println("Please add task description");
-        dataFromFileArray[dataFromFileArray.length-1][0] = scan.nextLine();
-        dataFromFileArray[dataFromFileArray.length-1][1] = dateAddAndValidation();
-        dataFromFileArray[dataFromFileArray.length-1][2] = importanceAddAndValidation();
+        dataFromFileArray[dataFromFileArray.length - 1][0] = scan.nextLine().trim();
+        dataFromFileArray[dataFromFileArray.length - 1][1] = dateAddAndValidation();
+        dataFromFileArray[dataFromFileArray.length - 1][2] = importanceAddAndValidation();
         return dataFromFileArray;
     }
 
     private static String dateAddAndValidation() {         //In this task I want to validate with loops, Array and NumberUtils and without date API or regex
         Scanner scan = new Scanner(System.in);
         String date;
-        while(true) {
+        while (true) {
             System.out.println("Please add task due date(YYYY-MM-DD)");
             date = scan.nextLine().trim();
             String[] dateArray = date.split("-");
-            if(dateArray.length != 3){
-                System.out.print("Date format is incorrect. ");
+            if (dateArray.length != 3) {
+                System.out.println(RED + "Date format is incorrect. " + RESET);
                 continue;
             }
-            if(dateArray[0].length() != 4 || !NumberUtils.isDigits(dateArray[0])){
-                System.out.print("You have given incorrect Year format (not all of the data are numbers or there are too many/ too few numbers). ");
+            if (dateArray[0].length() != 4 || !NumberUtils.isDigits(dateArray[0])) {
+                System.out.println(RED + "You have typed incorrect Year format (not all of the data are numbers or there are too many/ too few signs). " + RESET);
                 continue;
             }
-            if(dateArray[1].length() != 2 || !NumberUtils.isDigits(dateArray[1])){
-                System.out.print("You have given incorrect Month format (not all of the data are numbers or there are too many/ too few numbers). ");
+            if (dateArray[1].length() != 2 || !NumberUtils.isDigits(dateArray[1])) {
+                System.out.println(RED + "You have given incorrect Month format (not all of the signs are numbers or there are too many/ too few signs). " + RESET);
                 continue;
             }
-            if(Integer.parseInt(dateArray[1]) < 1 || Integer.parseInt(dateArray[1]) >12){
-                System.out.print("You have given incorrect Month date (Month date shouldn't be greater than 12 or less than 1). ");
+            if (Integer.parseInt(dateArray[1]) < 1 || Integer.parseInt(dateArray[1]) > 12) {
+                System.out.println(RED + "You have given incorrect Month date (Month date shouldn't be greater than 12 or less than 1). " + RESET);
                 continue;
             }
-            if(!NumberUtils.isDigits(dateArray[2])){
-                System.out.print("You have given incorrect Day format (not all of the data are numbers). ");
+            if (dateArray[2].length() != 2 || !NumberUtils.isDigits(dateArray[2])) {
+                System.out.println(RED + "You have given incorrect Day format (not all of the signs are numbers or there are too many/ too few signs). " + RESET);
                 continue;
             }
-            if(Integer.parseInt(dateArray[2]) < 1 || Integer.parseInt(dateArray[2]) >31){
-                System.out.print("You have given incorrect Day date (Day date shouldn't be greater than 31 or less than 1). ");
+            if (Integer.parseInt(dateArray[2]) < 1 || Integer.parseInt(dateArray[2]) > 31) {
+                System.out.println(RED + "You have typed incorrect Day date (Day date shouldn't be greater than 31 or less than 1). " + RESET);
                 continue;
             }
-           break;
+            break;
         }
         return date;
     }
+
     private static String importanceAddAndValidation() {
         Scanner scan = new Scanner(System.in);
         String importance;
-        while(true){
+        while (true) {
             System.out.println("Is your task important: true/false");
             importance = scan.nextLine().trim().toLowerCase();
-            if(!StringUtils.equalsAny(importance,"true", "false")){
-                System.out.println("You have given incorrect data (neither true nor false). Please try once again. ");
+            if (!StringUtils.equalsAny(importance, "true", "false")) {
+                System.out.println(RED + "You have given incorrect data (neither true nor false). Please try once again. " + RESET);
                 continue;
             }
             break;
@@ -147,30 +146,29 @@ public class TaskManager {
 
     private static String[][] remove(String[][] dataFromFileArray) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please select number to remove from the list. If you want to display list type \'list\', if you want to quit remove option type \'quit\'.");
+        System.out.println("Please select number to remove from the list. If you want to display list type 'list', if you want to quit remove option type 'quit'.");
         String numberToRemove;
-        while (true){
+        while (true) {
             numberToRemove = scan.nextLine().trim();
-            if(numberToRemove.equalsIgnoreCase("list")){
+            if (numberToRemove.equalsIgnoreCase("list")) {
                 list(dataFromFileArray);
                 System.out.println("Please select number to remove from the list");
                 continue;
             }
-            if(numberToRemove.equalsIgnoreCase("quit")){
-                System.out.println("You are quitting the option.");
+            if (numberToRemove.equalsIgnoreCase("quit")) {
+                System.out.println("You are quitting the remove option.");
                 break;
             }
-            if(!StringUtils.isNumeric(numberToRemove)){
-                System.out.println("Inserted data is not a number. Please select number to remove from the list. If you want to display list type \'list\', if you want to quit remove option type \'quit\'.");
+            if (!StringUtils.isNumeric(numberToRemove)) {
+                System.out.println(RED + "Typed data is not a number. Please select number to remove from the list." + RESET + " If you want to display list type 'list', if you want to quit remove option type 'quit'.");
                 continue;
             }
-            try{
-                dataFromFileArray = ArrayUtils.remove(dataFromFileArray, Integer.parseInt(numberToRemove));
-                System.out.println("Entry number " + numberToRemove + " was removed");
+            try {
+                dataFromFileArray = ArrayUtils.remove(dataFromFileArray, Integer.parseInt(numberToRemove));  //NEW mogę index z 2-wym też usunąć
+                System.out.println(YELLOW + "Entry number " + numberToRemove + " was removed" + RESET);
                 break;
-            } catch (IndexOutOfBoundsException e){
-                System.out.println("Given number doesn't exist. Please type a number from the list. If you want to display list type \'list\', if you want to quit remove option type \'quit\'.");
-                continue;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(RED + "Given number doesn't exist. Please type a number from the list." + RESET + " If you want to display list type 'list', if you want to quit remove option type 'quit'.");
             }
         }
         return dataFromFileArray;
@@ -178,31 +176,32 @@ public class TaskManager {
 
     private static void list(String[][] dataFromFileArray) {
         int counter = 0;
-        System.out.println(CYAN + "List: ");
+        System.out.println(PURPLE + "List: " + RESET);
         for (String[] array : dataFromFileArray) {
-            System.out.print(counter + " : ");
+            System.out.print(" " + counter + " : ");
             for (String index : array) {
                 System.out.print(index + " ");
             }
             System.out.println("\b");
+            counter++;
         }
         System.out.println();
     }
 
     private static void exit(String[][] dataFromFileArray, String csvFile) {
-        try(FileWriter fileWriter = new FileWriter(csvFile)) {
+        try (FileWriter fileWriter = new FileWriter(csvFile)) {
             for (int i = 0; i < dataFromFileArray.length; i++) {
                 for (int j = 0; j < dataFromFileArray[i].length; j++) {
-                    if(j == dataFromFileArray[i].length - 1)
+                    if (j == dataFromFileArray[i].length - 1)
                         fileWriter.append(dataFromFileArray[i][j]).append("\n");
                     else
                         fileWriter.append(dataFromFileArray[i][j]).append(", ");
                 }
             }
+            System.out.println(CYAN_UNDERLINED + "All data were saved in a file");
         } catch (IOException e) {
-            System.out.println("There was a problem with finding or writing to a file. Check directory");
+            System.out.println(RED + "There was a problem with finding or writing to a file. Check directory" + RESET);
             e.printStackTrace();
-            return;
         }
 
     }
